5ec5894b1b4b0e3fec2cb280db8ff4ff28cf90aa
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-08-21 13:44:02 -0300

    style:(footer) imporando o styled-compoent de texto

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,16 @@
 // Footer.tsx
 import React from 'react'
-import { FooterContainer } from './styles'
+import { FooterContainer, FooterText } from './styles'
 
 export const Footer: React.FC = () => {
     const currentYear = new Date().getFullYear()
 
     return (
         <FooterContainer role="contentinfo" aria-label="Rodapé do site">
-            <p>© {currentYear} MeuPortfólio. Todos os direitos reservados.
+            <FooterText>© {currentYear} MeuPortfólio. Todos os direitos reservados.
                 <br />
                 Desenvolvido por <a href="https://github.com/ronald1134" target="_blank">Ronald Oliveira</a>
-            </p>
+            </FooterText>
         </FooterContainer>
     )
 }
