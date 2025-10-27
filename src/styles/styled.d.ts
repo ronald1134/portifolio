d4490db957cbb6103e5eccb5a38dd84d9a3a9f2a
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-10-26 22:21:05 -0300

    refactor(types): remover comentários desnecessários no arquivo styled.d.ts

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,8 +1,6 @@
-// augmentação de tipos para styled-components
 import "styled-components";
 import { ThemeType  } from "./themes/default";
 
 declare module "styled-components" {
-    // informa ao TS o formato de props.theme
     export interface DefaultTheme extends ThemeType  { }
 }
